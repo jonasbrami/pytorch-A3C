261f6d853c529084c95d0feebfdb49a3a92383d9
Jonas <jonasbrami@gmail.com>
2020-08-19 19:22:38 +0300

    first try

--- discrete_A3C.py
+++ discrete_A3C.py
@@ -1,10 +1,3 @@
-"""
-Reinforcement Learning (A3C) using Pytroch + multiprocessing.
-The most simple implementation for continuous action.
-
-View more on my Chinese tutorial page [莫烦Python](https://morvanzhou.github.io/).
-"""
-
 import torch
 import torch.nn as nn
 from utils import v_wrap, set_init, push_and_pull, record
@@ -13,35 +6,64 @@ import torch.multiprocessing as mp
 from shared_adam import SharedAdam
 import gym
 import os
+import retro
+import numpy as np
 os.environ["OMP_NUM_THREADS"] = "1"
 
 UPDATE_GLOBAL_ITER = 5
 GAMMA = 0.9
 MAX_EP = 3000
+KERNEL_SIZE = 3 
 
-env = gym.make('CartPole-v0')
-N_S = env.observation_space.shape[0]
+
+
+env = retro.make('SonicTheHedgehog-Sms')
+N_S = env.observation_space.shape[2]
 N_A = env.action_space.n
-
+env.close()
 
 class Net(nn.Module):
     def __init__(self, s_dim, a_dim):
         super(Net, self).__init__()
+        
+        # self.extract_features_net = nn.Sequential(nn.Conv2d(N_S, 32, KERNEL_SIZE, stride=2, padding=1),#torch.Size([1, 3, 192, 256])
+        #                                  nn.ReLU(),#torch.Size([1, 32, 96, 128])
+        #                                  nn.Conv2d(32, 32, KERNEL_SIZE, stride=2, padding=1),
+        #                                  nn.ReLU(),#torch.Size([1, 32, 48, 64])
+        #                                  nn.Conv2d(32, 32, KERNEL_SIZE, stride=2, padding=1),
+        #                                  nn.ReLU(),#torch.Size([1, 32, 24, 32])
+        #                                  nn.Conv2d(32, 32, KERNEL_SIZE, stride=2, padding=1),
+        #                                  nn.ReLU()) #torch.Size([1, 32, 12, 16])
+                                         
         self.s_dim = s_dim
         self.a_dim = a_dim
-        self.pi1 = nn.Linear(s_dim, 128)
-        self.pi2 = nn.Linear(128, a_dim)
-        self.v1 = nn.Linear(s_dim, 128)
-        self.v2 = nn.Linear(128, 1)
-        set_init([self.pi1, self.pi2, self.v1, self.v2])
+        
+        self.conv1 = nn.Conv2d(3 , 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+
+        self.actor = nn.Sequential(nn.Linear(256, 128),
+                                   nn.Tanh(),
+                                   nn.Linear(128, a_dim))
+        self.critic = nn.Sequential(nn.Linear(256, 128),
+                                    nn.Tanh(),
+                                    nn.Linear(128, 1))
+        
+
         self.distribution = torch.distributions.Categorical
+        self.lstm = nn.LSTMCell(32 * 12 * 16 , 256)
+        self.hx , self.cx = (None, None)
 
     def forward(self, x):
-        pi1 = torch.tanh(self.pi1(x))
-        logits = self.pi2(pi1)
-        v1 = torch.tanh(self.v1(x))
-        values = self.v2(v1)
-        return logits, values
+        #extracted_features = self.extract_features_net(x)
+        x = nn.ReLU(self.conv1(x))
+        x = nn.ReLU(self.conv2(x))
+        x = nn.ReLU(self.conv3(x))
+        x = nn.ReLU(self.conv4(x))
+        (self.hx, self.cx) = self.lstm( x, (self.hx, self.cx) )
+        print('tata',self.hx.shape)
+        return self.actor(self.hx), self.critic(self.hx)
 
     def choose_action(self, s):
         self.eval()
@@ -71,7 +93,7 @@ class Worker(mp.Process):
         self.g_ep, self.g_ep_r, self.res_queue = global_ep, global_ep_r, res_queue
         self.gnet, self.opt = gnet, opt
         self.lnet = Net(N_S, N_A)           # local network
-        self.env = gym.make('CartPole-v0').unwrapped
+        self.env = retro.make('SonicTheHedgehog-Sms')
 
     def run(self):
         total_step = 1
@@ -80,6 +102,7 @@ class Worker(mp.Process):
             buffer_s, buffer_a, buffer_r = [], [], []
             ep_r = 0.
             while True:
+                s = np.moveaxis(s,-1,0) #channel first
                 if self.name == 'w00':
                     self.env.render()
                 a = self.lnet.choose_action(v_wrap(s[None, :]))
@@ -104,13 +127,14 @@ class Worker(mp.Process):
 
 
 if __name__ == "__main__":
-    gnet = Net(N_S, N_A)        # global network
+    #import pdb;pdb.set_trace()
+    gnet = Net(N_S, N_A)         # global network
     gnet.share_memory()         # share the global parameters in multiprocessing
     opt = SharedAdam(gnet.parameters(), lr=1e-4, betas=(0.92, 0.999))      # global optimizer
     global_ep, global_ep_r, res_queue = mp.Value('i', 0), mp.Value('d', 0.), mp.Queue()
 
     # parallel training
-    workers = [Worker(gnet, opt, global_ep, global_ep_r, res_queue, i) for i in range(mp.cpu_count())]
+    workers = [Worker(gnet, opt, global_ep, global_ep_r, res_queue, i) for i in range(1)]
     [w.start() for w in workers]
     res = []                    # record episode reward to plot
     while True:
